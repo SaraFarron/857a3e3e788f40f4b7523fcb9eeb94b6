0c18602c84668a344ae80ae8f4a06028b314965a
Nikita Syulin <nikita_sulin@idpowers.com>
2021-08-23 18:11:45 +0700

    work with celery task

--- server/core/models.py
+++ server/core/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from .tasks import generate_picture
 
 
 class Function(models.Model):
+    id = models.AutoField(primary_key=True)
     statement = models.CharField(max_length=128, verbose_name='Функция')
     dt = models.IntegerField(verbose_name='Шаг t, часы')
     interval = models.IntegerField(verbose_name='Интервал t, дней')
@@ -17,6 +17,8 @@ class Function(models.Model):
         verbose_name = _('функцию')
         verbose_name_plural = _('функции')
 
-    def save(self, *args, **kwargs):
-        self.image = generate_picture.delay(self.statement, self.dt, self.interval)
+    def save(self, *args, **kwargs):  # if need_generation: generate_picture...
+        from .tasks import generate_picture
+
         super(Function, self).save(*args, **kwargs)
+        generate_picture.delay(self.statement, self.dt, self.interval, self.id)
--- server/core/tasks.py
+++ server/core/tasks.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import re
 from datetime import datetime
+from time import sleep
+from django.core.files.images import ImageFile
+from .models import Function
 
 
 replacements = {
@@ -42,12 +45,18 @@ def string2func(string):
 
 
 @shared_task()
-def generate_picture(statement, dt, interval):
+def generate_picture(statement, dt, interval, func_id):
     x = np.linspace(datetime.now().day - interval, datetime.now().day, num=dt*interval)
     y = [string2func(statement)(t) for t in x]
     plt.plot(x, y)
     image = BytesIO()
-    plt.savefig(image)
-    return image
+    plt.savefig(image, format='png')
+    picture = ImageFile(image)
+    while not Function.objects.filter(id=func_id):
+        sleep(1)
+        print(f'id = {func_id}, dt = {dt}, statement = {statement}, interval = {interval}')
+    else:
+        Function.objects.get(id=func_id).update(graph=picture)
+
 
 # чтобы селери делал то что написано в таске надо добавить к команде --pool=solo
